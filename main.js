3a96e99ec0bc8860a181938d2176db97b4565c9a
JHIH-LEI <vicky88888@gmail.com>
2021-06-21 21:19:31 +0800

    Rewrite Comment

--- main.js
+++ main.js
@@ -29,19 +29,19 @@ const view = {
     cards.map(card => {
       // 如果是背面
       if (card.classList.contains('back')) {
+        // 回傳正面(牌面內容)
         card.classList.remove('back')
         card.innerHTML = this.getCardContent(card.dataset.index)
         return
       }
-      // 回傳正面(牌面內容)
-      //如果是正面
+      //如果是正面，回傳背面，並且清空卡牌內容
       card.classList.add('back')
       card.innerHTML = null
-      // 回傳背面
     })
   },
   getCardContent(index) {
     // 餘數不能大於除數，所以餘數的最大值就是除數-1
+    // 運用餘數來得到卡牌數字，用商數得到卡牌花色
     const number = this.transformNumber((index % 13) + 1)
     const symbol = Symbols[Math.floor(index / 13)]
     return `<p>${number}</p>
@@ -140,10 +140,10 @@ const controller = {
         // 判斷配對是否成功
         if (model.isRevealedCardsMatched()) {
           //如果配對成功
-          //卡片維持翻面狀態(paired樣式)
           view.renderScore(model.score += 10)
           view.appendAddScoreAnimation()
           this.currentState = GAME_STATE.CardsMatched
+          //卡片維持翻面狀態(paired樣式)
           view.paired(...model.revealedCards)
           model.revealedCards = []
           this.currentState = GAME_STATE.FirstCardAwaits
@@ -189,64 +189,3 @@ const card = document.querySelectorAll('.card').forEach(card => {
     controller.dispatchCardAction(card)
   })
 })
-
-
-
-
-// dispatchCardAction(card) {
-//   if (!card.classList.contains('back')) {
-//     return
-//   }
-//   switch (this.currentState) {
-//     case GAME_STATE.FirstCardAwaits:
-//       view.flipCards(card)
-//       model.revealedCards.push(card)
-//       this.currentState = GAME_STATE.SecondCardAwaits
-//       break
-//     case GAME_STATE.SecondCardAwaits:
-//       view.renderTriedTimes(model.triedTimes += 1)
-//       view.flipCards(card)
-//       model.revealedCards.push(card)
-//       // 判斷配對是否成功
-//       if (model.isRevealedCardsMatched()) {
-//         this.currentState = GAME_STATE.CardsMatched
-//       } else {
-//         this.currentState = GAME_STATE.CardsMatchFailed
-//       }
-//       break
-//   }
-//   switch (this.currentState) {
-//     case GAME_STATE.CardsMatched:
-//       view.renderScore(model.score += 10)
-//       view.paired(...model.revealedCards)
-//       model.revealedCards = []
-//       this.currentState = GAME_STATE.FirstCardAwaits
-//       break
-//     case GAME_STATE.CardsMatchFailed:
-//       setTimeout(
-//         this.resetCards
-//         , 1000)
-//       break
-//   }
-//   console.log('this.currentState', this.currentState)
-// },
-
-
-// 隨機產生一組數字陣列
-// 將數字陣列傳入產生器
-// const totalCardNum = []
-// function generateTotalNum() {
-//   let randomNum = Math.floor(Math.random() * totoalCards)
-//   while (!totalCardNum.length === totoalCards) {
-//     randomNum = Math.floor(Math.random() * totoalCards)
-//     if (!totalCardNum.some(cardNum => cardNum === randomNum)) {
-//       totalCardNum.push(num)
-//     }
-//   }
-//   console.log(totalCardNum)
-// }
-
-// 隨機產生數字
-// 檢查是否重複
-// 把數字推到陣列裡面
-// 將傳入getCardElement()
